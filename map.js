d8d9d041f1fd5fd123c81eee333bb1277fba840d
J3 <gitlab@nevs.me>
2023-12-09 15:31:59 +0100

    added tooltips

--- map.js
+++ map.js
@@ -22,11 +22,53 @@ document.addEventListener('DOMContentLoaded', function() {
     // Set the view to the center of the image
     map.fitBounds(bounds);
 
-    // // Adding markers
-    // var iconOptions = {
-    //     // Define the appearance of the icon here
-    // };
+    var iconOptions = L.divIcon({
+        className: 'custom-icon', // Custom class name to target the div
+        html: '<img src="./icon.png" style="width:24px; height:24px;">',
+        iconSize: [24, 24],
+        iconAnchor: [12, 12],
+        popupAnchor: [0, -12]
+    });
+    
+    // Add custom CSS
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.custom-icon { border: none !important; background: none !important; }';
+    document.getElementsByTagName('head')[0].appendChild(style);
+    
 
-    // var marker = L.marker([coordinateX, coordinateY], {icon: iconOptions}).addTo(map);
-    // marker.bindPopup('Your tooltip text here');
+    // Array of marker data
+    var markers = [
+        { imgCoords: [2794, 2594], tooltip: "J 3 3 3's village 1" },
+        { imgCoords: [2394, 2533], tooltip: "Hwajun's village 2" },
+        { imgCoords: [2603.5, 2558.5], tooltip: "Hwajun's village 1" },
+        { imgCoords: [2612.5, 2286.5], tooltip: "loaep's village 1" },
+        { imgCoords: [2579, 2463], tooltip: "River bridge" },
+        { imgCoords: [2802, 2002], tooltip: "Hostile camp" },
+        { imgCoords: [3132.5, 2325.5], tooltip: "Cows" },
+        { imgCoords: [3251.5, 2394], tooltip: "Cows" },
+        { imgCoords: [3618, 2764], tooltip: "Boss" },
+        { imgCoords: [3284, 3484], tooltip: "Boss" },
+        { imgCoords: [3821, 3572], tooltip: "Boss" },
+        { imgCoords: [4658, 4292], tooltip: "Boss" },
+        { imgCoords: [4724, 3965], tooltip: "Boss" },
+        { imgCoords: [3385, 3478], tooltip: "Great Desert bridge start point" },
+        { imgCoords: [3788, 3699], tooltip: "Great Desert bridge continued" },
+        { imgCoords: [4830, 4421], tooltip: "Desert safehouse" },
+        // ... add as many markers as you need
+    ];
+
+    // Loop over the array to create markers
+    markers.forEach(function(marker) {
+        var coords = map.unproject(marker.imgCoords, map.getMaxZoom()-1);
+        L.marker(coords, {icon: iconOptions})
+            .addTo(map)
+            .bindPopup(marker.tooltip);
+    });
+
+    map.on('click', function(e) {
+        var clickedPoint = map.project(e.latlng, map.getMaxZoom()-1);
+        console.log('Clicked coordinates: ', clickedPoint.x+',', clickedPoint.y);
+    });
+    
 });
